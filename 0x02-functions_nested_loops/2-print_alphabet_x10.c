f5cfb6904b28c014cf44e7e953b36474704a5ec7
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2023-03-21 12:45:37 -0700

    print alph

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,15 +5,19 @@
  *
  * Return - 0
  */
-void print_alphabet_x10(void) {
-  int b = 0;
-  while (b < 10) {
-    char a = 'a';
-    while (a <= 'z') {
-      _putchar(a);
-      a++;
-    }
-    _putchar('\n');
-    b++;
-  }
+void print_alphabet_x10(void)
+{
+int b = 0;
+while (b < 10)
+{
+char a = 'a';
+while (a <= 'z')
+{
+_putchar(a);
+a++;
 }
+_putchar('\n');
+b++;
+}
+}
+
